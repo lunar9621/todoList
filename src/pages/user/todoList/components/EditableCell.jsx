922994acd495709bd9e4db4c0c712c35a33094ea
lunar9621 <546604583@qq.com>
2020-03-02 16:39:04 +0800

    update

--- src/pages/user/todoList/components/EditableCell.jsx
+++ src/pages/user/todoList/components/EditableCell.jsx
@@ -1,50 +1,50 @@
-import {Input,Form} from 'antd';
+import { Input, Form } from 'antd';
 import { FormattedMessage, formatMessage } from 'umi-plugin-react/locale';
 import React, { Component } from 'react';
 import { Link } from 'umi';
 import { connect } from 'dva';
 const EditableContext = React.createContext();
 class EditableCell extends React.Component {
-    state = {
-      editing: false,
-    };
-  
-    toggleEdit = () => {
-      const editing = !this.state.editing;
-      this.setState({ editing }, () => {
-        if (editing) {
-          this.input.focus();
-        }
-      });
-    };
-  
-    save = e => {
-      const { record, handleSave } = this.props;
-      this.props.form.validateFields((error, values) => {
-        if (error && error[e.currentTarget.id]) {
-          return;
-        }
-        this.toggleEdit();
-        handleSave({ ...record, ...values });
-      });
-    };
-  
-    renderCell = form => {
-      const { children, dataIndex, record, title } = this.props;
-      const { editing } = this.state;
-      return editing ? (
-        <Form.Item style={{ margin: 0 }}>
-          {this.props.form.getFieldDecorator(dataIndex, {
-            rules: [
-              {
-                required: true,
-                message: `${title} is required.`,
-              },
-            ],
-            initialValue: record[dataIndex],
-          })(<Input ref={node => (this.input = node)} onPressEnter={this.save} onBlur={this.save} />)}
-        </Form.Item>
-      ) : (
+  state = {
+    editing: false,
+  };
+
+  toggleEdit = () => {
+    const editing = !this.state.editing;
+    this.setState({ editing }, () => {
+      if (editing) {
+        this.input.focus();
+      }
+    });
+  };
+
+  save = e => {
+    const { record, handleSave } = this.props;
+    this.props.form.validateFields((error, values) => {
+      if (error && error[e.currentTarget.id]) {
+        return;
+      }
+      this.toggleEdit();
+      handleSave({ ...record, ...values });
+    });
+  };
+
+  renderCell = form => {
+    const { children, dataIndex, record, title } = this.props;
+    const { editing } = this.state;
+    return editing ? (
+      <Form.Item style={{ margin: 0 }}>
+        {this.props.form.getFieldDecorator(dataIndex, {
+          rules: [
+            {
+              required: true,
+              message: `${title} is required.`,
+            },
+          ],
+          initialValue: record[dataIndex],
+        })(<Input ref={node => (this.input = node)} onPressEnter={this.save} onBlur={this.save} />)}
+      </Form.Item>
+    ) : (
         <div
           className="editable-cell-value-wrap"
           style={{ paddingRight: 24 }}
@@ -53,28 +53,28 @@ class EditableCell extends React.Component {
           {children}
         </div>
       );
-    };
-    render() {
-        const {
-          editable,
-          dataIndex,
-          title,
-          record,
-          index,
-          handleSave,
-          children,
-          ...restProps
-        } = this.props;
-        return (
-          <td {...restProps}>
-            {editable ? (
-              <EditableContext.Consumer>{this.renderCell}</EditableContext.Consumer>
-            ) : (
-              children
-            )}
-          </td>
-        );
-    }
+  };
+  render() {
+    const {
+      editable,
+      dataIndex,
+      title,
+      record,
+      index,
+      handleSave,
+      children,
+      ...restProps
+    } = this.props;
+    return (
+      <td {...restProps}>
+        {editable ? (
+          <EditableContext.Consumer>{this.renderCell}</EditableContext.Consumer>
+        ) : (
+            children
+          )}
+      </td>
+    );
+  }
 }
 
 export default Form.create()(EditableCell);
--- src/pages/user/todoList/index.jsx
+++ src/pages/user/todoList/index.jsx
@@ -1,11 +1,11 @@
-import {Input,Form,Table,Radio,Popconfirm,Button} from 'antd';
+import { Input, Form, Table, Radio, Popconfirm, Button } from 'antd';
 import { FormattedMessage, formatMessage } from 'umi-plugin-react/locale';
 import React, { Component } from 'react';
 import { Link } from 'umi';
 import { connect } from 'dva';
 import EditableCell from './components/EditableCell';
 import styles from './style.less';
-const {Search}=Input;
+const { Search } = Input;
 const EditableContext = React.createContext();
 
 const EditableRow = ({ form, index, ...props }) => (
@@ -15,23 +15,23 @@ const EditableRow = ({ form, index, ...props }) => (
 );
 
 const EditableFormRow = Form.create()(EditableRow);
- class TodoList extends Component {
-  
+class TodoList extends Component {
+
   state = {
-    length:0,
-    selectedRowKeys:[],
-    storeAll:[],
-    eventList:[],
-    status:'All',
+    length: 0,
+    selectedRowKeys: [],
+    storeAll: [],
+    eventList: [],
+    status: 'All',
   };
-  columns=[ {
+  columns = [{
     dataIndex: 'name',
     width: '60%',
     editable: true,
   },
   {
     dataIndex: 'operation',
-    width:'20%',
+    width: '20%',
     render: (text, record) =>
       this.state.eventList.length >= 1 ? (
         <Popconfirm title={formatMessage({
@@ -44,22 +44,22 @@ const EditableFormRow = Form.create()(EditableRow);
 
   handleDelete = index => {
     const storeAll = [...this.state.storeAll];
-    this.setState({ storeAll: storeAll.filter(item => item.index !== index) },()=>{this.changeEventList()});
+    this.setState({ storeAll: storeAll.filter(item => item.index !== index) }, () => { this.changeEventList() });
   };
 
   addEvent = value => {
-    let tmpstoreAll=this.state.storeAll;
+    let tmpstoreAll = this.state.storeAll;
     tmpstoreAll.push({
-    index:this.state.length,
-    name:value,
-    done:false,
-  })
-  let tmplength=this.state.length+1;
-  this.setState({
-    length:tmplength,
-    storeAll:tmpstoreAll,
-  },()=>{this.changeEventList()});
-  this.props.form.resetFields(['input']);
+      index: this.state.length,
+      name: value,
+      done: false,
+    })
+    let tmplength = this.state.length + 1;
+    this.setState({
+      length: tmplength,
+      storeAll: tmpstoreAll,
+    }, () => { this.changeEventList() });
+    this.props.form.resetFields(['input']);
   }
 
   handleSave = row => {
@@ -70,87 +70,87 @@ const EditableFormRow = Form.create()(EditableRow);
       ...item,
       ...row,
     });
-     this.setState({
-       storeAll:  newData
-      },()=>{this.changeEventList()});
+    this.setState({
+      storeAll: newData
+    }, () => { this.changeEventList() });
   };
 
-  onChange=e=>{
+  onChange = e => {
     console.log(`radio checked:${e.target.value}`);
     let status;
-   if(e.target.value=='a'){
-       status='All';
-   }else if(e.target.value=='b'){
-       status='Active';
-   }else{
-       status='Completed';
-   }
-   this.setState({
-     status,
-   },()=>this.changeEventList());
+    if (e.target.value == 'a') {
+      status = 'All';
+    } else if (e.target.value == 'b') {
+      status = 'Active';
+    } else {
+      status = 'Completed';
+    }
+    this.setState({
+      status,
+    }, () => this.changeEventList());
   }
 
-  changeEventList(){
-    const {status,storeAll,selectedRowKeys}=this.state;
-    let eventList,tempselectedRowKeys=[],flag=0;
-    if(status === 'All'){
-     eventList=storeAll;
-    }else if(status === 'Active'){
-      eventList=storeAll.filter(item=>item.done === false);
-    }else{
-      eventList=storeAll.filter(item=>item.done === true);
+  changeEventList() {
+    const { status, storeAll, selectedRowKeys } = this.state;
+    let eventList, tempselectedRowKeys = [], flag = 0;
+    if (status === 'All') {
+      eventList = storeAll;
+    } else if (status === 'Active') {
+      eventList = storeAll.filter(item => item.done === false);
+    } else {
+      eventList = storeAll.filter(item => item.done === true);
     }
-    console.log('eventList',storeAll,eventList);
-    for(let i=0;i<eventList.length;i++){
-      if(eventList[i].done===true){
-      tempselectedRowKeys.push(flag);
+    console.log('eventList', storeAll, eventList);
+    for (let i = 0; i < eventList.length; i++) {
+      if (eventList[i].done === true) {
+        tempselectedRowKeys.push(flag);
       }
       flag++;
     }
     this.setState({
       eventList,
-      selectedRowKeys:tempselectedRowKeys,
+      selectedRowKeys: tempselectedRowKeys,
     });
   }
 
-  onSelectChange=(innerselectedRowKeys, selectedRows)=>{
+  onSelectChange = (innerselectedRowKeys, selectedRows) => {
     console.log(`selectedRowKeys: ${innerselectedRowKeys}`, 'selectedRows: ', selectedRows);
-    const {eventList}=this.state;
-        let realselectedKeys=[];
-        for(let i=0;i<selectedRows.length;i++){
-          realselectedKeys.push(selectedRows[i].index);
-        }
-        for(let i=0;i<eventList.length;i++){
-          if(realselectedKeys.indexOf(eventList[i].index)==-1){
-            eventList[i].done=false;
-          }else{
-            eventList[i].done=true;
-          }
-        }
-        this.setState({
-          selectedRowKeys:realselectedKeys,
-        })
-        console.log('afterselectedRows:',selectedRows,eventList);
+    const { eventList } = this.state;
+    let realselectedKeys = [];
+    for (let i = 0; i < selectedRows.length; i++) {
+      realselectedKeys.push(selectedRows[i].index);
+    }
+    for (let i = 0; i < eventList.length; i++) {
+      if (realselectedKeys.indexOf(eventList[i].index) == -1) {
+        eventList[i].done = false;
+      } else {
+        eventList[i].done = true;
+      }
+    }
+    this.setState({
+      selectedRowKeys: realselectedKeys,
+    })
+    console.log('afterselectedRows:', selectedRows, eventList);
   }
 
-  clearCompleted=()=>{
-     const {storeAll}=this.state;
-      let tmpstoreAll=storeAll.filter(item=>item.done===false);
-     this.setState({
-       storeAll:tmpstoreAll,
-     },()=>{this.changeEventList()})
+  clearCompleted = () => {
+    const { storeAll } = this.state;
+    let tmpstoreAll = storeAll.filter(item => item.done === false);
+    this.setState({
+      storeAll: tmpstoreAll,
+    }, () => { this.changeEventList() })
   }
 
   render() {
-    const { storeAll,eventList,selectedRowKeys } = this.state;
+    const { storeAll, eventList, selectedRowKeys } = this.state;
 
     const { getFieldDecorator } = this.props.form;
-    console.log("render",eventList);
+    console.log("render", eventList);
     const rowSelection = {
       selectedRowKeys,
-      onChange:this.onSelectChange,
+      onChange: this.onSelectChange,
       getCheckboxProps: record => ({
-       done:false,
+        done: false,
       }),
     };
     const components = {
@@ -177,41 +177,41 @@ const EditableFormRow = Form.create()(EditableRow);
     return (
       <div className={styles.main}>
         <Form>
-        <Form.Item style={{margin:0}}>
-          {getFieldDecorator('input')(
-          <Search
-            placeholder={formatMessage({
-              id: 'What-needs-to-be-done',
-            })}
-            enterButton={formatMessage({
-              id: 'Add',
-            })}
-            size="large"
-            onSearch={value=>this.addEvent(value)}
-          />)}
-        </Form.Item>
-        <Table 
-          components={components}
-          rowClassName={() => 'editable-row'}
-          showHeader={false}
-          bordered={false}
-          pagination={ false } 
-          scroll={{ y: 400 }}
-          dataSource={eventList}
-          columns={columns}
-          rowSelection={rowSelection}
+          <Form.Item style={{ margin: 0 }}>
+            {getFieldDecorator('input')(
+              <Search
+                placeholder={formatMessage({
+                  id: 'What-needs-to-be-done',
+                })}
+                enterButton={formatMessage({
+                  id: 'Add',
+                })}
+                size="large"
+                onSearch={value => this.addEvent(value)}
+              />)}
+          </Form.Item>
+          <Table
+            components={components}
+            rowClassName={() => 'editable-row'}
+            showHeader={false}
+            bordered={false}
+            pagination={false}
+            scroll={{ y: 400 }}
+            dataSource={eventList}
+            columns={columns}
+            rowSelection={rowSelection}
           />
           <span>{eventList.length}<FormattedMessage id="items-left" /></span>
-          <Radio.Group onChange={this.onChange} defaultValue="a" style={{display:'inline-block',marginLeft:"10px"}}>
+          <Radio.Group onChange={this.onChange} defaultValue="a" style={{ display: 'inline-block', marginLeft: "10px" }}>
             <Radio.Button value="a"><FormattedMessage id="All" /></Radio.Button>
             <Radio.Button value="b"><FormattedMessage id="Active" /></Radio.Button>
             <Radio.Button value="c"><FormattedMessage id="Completed" /></Radio.Button>
           </Radio.Group>
-          <Button onClick={this.clearCompleted} style={{display:'float',float:'right'}}>
-             <FormattedMessage id="clearCompleted" />
+          <Button onClick={this.clearCompleted} style={{ display: 'float', float: 'right' }}>
+            <FormattedMessage id="clearCompleted" />
           </Button>
-       </Form>
-      </div> 
+        </Form>
+      </div>
     );
   }
 }
